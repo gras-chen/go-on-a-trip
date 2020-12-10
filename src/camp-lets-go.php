cd9f274387d416979c072e94556b2d8fbbffd4d3
RuYi-Chen <69340316+gras-chen@users.noreply.github.com>
2020-12-11 01:08:26 +0800

    update open data

--- src/camp-lets-go.php
+++ src/camp-lets-go.php
@@ -2,7 +2,7 @@
 	header("Access-Control-Allow-Origin:*");
 	$url = "https://data.coa.gov.tw/Service/OpenData/ODwsv/ODwsvTravelStay.aspx";
 	$mydata = file_get_contents($url);
-	// echo json_encode($mydata);
+	echo json_encode($mydata);
 	echo $mydata;
 
 ?>
